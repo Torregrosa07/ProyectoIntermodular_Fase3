7a7981f323d290958b9ea771f538ce6a886f3fae
Santiago Torregrosa <santiago.torre07a@gmail.com>
2025-04-01 09:26:17 +0200

    prueba

--- src/proyectointermodular_fase3/ProyectoIntermodular_Fase3.java
+++ src/proyectointermodular_fase3/ProyectoIntermodular_Fase3.java
@@ -15,6 +15,8 @@ public class ProyectoIntermodular_Fase3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hola mundo");
     }
     
 }
